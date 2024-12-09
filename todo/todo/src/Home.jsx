964c5497bfc740b0335250cd850d9aed087ef082
Apsara <paneruapsara831ap@gmail.com>
2024-12-09 13:30:23 +0545

    create todo list

---
+++ todo/todo/src/Home.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import Create from "./Create";
+
+export default function Home() {
+  const [todos, settodo] = useState([]);
+  return (
+    <div>
+      <h2>Todo List</h2>
+      <Create />
+      {todos.length === 0 ? (
+        <div>
+          <h2>No Record</h2>
+        </div>
+      ) : (
+        todos.map(todo =>(
+            <div>
+                {todo}
+            </div>
+        ))
+
+        )}
+    </div>
+  );
+}
